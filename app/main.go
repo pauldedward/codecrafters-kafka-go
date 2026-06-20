979b2a4e189f5fb5f1e8ccbb746c58c05a83ee20
Paul D Edward <edward2000ed@gmail.com>
2026-06-20 14:44:42 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,9 +13,17 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	fmt.Println("Listening on port 9092...")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			continue
+		}
+		response := []byte{0, 0, 0, 10, 0, 0, 0, 7}
+		conn.Write(response)
+		conn.Close()
 	}
+
 }
