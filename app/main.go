05859e6ee0ed65db40a266312a56d38ca62d2878
Paul D Edward <edward2000ed@gmail.com>
2026-06-20 15:57:38 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 
+	messageSize := []byte{0, 0, 0, 4}
+
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
@@ -21,20 +23,31 @@ func main() {
 			fmt.Println("Failed to accept connection")
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, messageSize)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, messageSize []byte) {
 	defer conn.Close()
 
 	message := make([]byte, 1024)
+
 	_, err := conn.Read(message)
 	if err != nil {
 		fmt.Println("Failed to read message")
 		return
 	}
-	conn.Write([]byte{0, 0, 0, 4})
+	//read message 6, 7 bytes as api version
+	apiVersion := message[6:8]
+
+	conn.Write(messageSize)
 	conn.Write(message[8:12])
+
+	apiVersionInt := int(apiVersion[0])<<8 + int(apiVersion[1])
+	if apiVersionInt < 0 || apiVersionInt > 4 {
+		conn.Write([]byte{0, 35})
+	} else {
+		conn.Write([]byte{0, 0})
+	}
 
 }
