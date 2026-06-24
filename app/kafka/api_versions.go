545ebf5cc7b947c7128958f0139b27f5f2695432
Paul D Edward <edward2000ed@gmail.com>
2026-06-24 08:08:25 +0530

    codecrafters submit [skip ci]

--- app/kafka/api_versions.go
+++ app/kafka/api_versions.go
@@ -6,7 +6,11 @@ import (
 	protocol "github.com/codecrafters-io/kafka-starter-go/app/protocol"
 )
 
-var supportedAPIVersions = []int16{0, 1, 2, 3, 4}
+var supportedAPIs = []int16{18, 75} // Supported APIs
+var supportedAPIVersionMap = map[int16][2]int16{
+	18: {0, 4},
+	75: {0, 2},
+}
 
 func contains(slice []int16, val int16) bool {
 	for _, v := range slice {
@@ -19,18 +23,26 @@ func contains(slice []int16, val int16) bool {
 
 func HandleAPIVersions(requestHeader RequestHeader, decoder *protocol.Decoder) ([]byte, error) {
 	encoder := protocol.NewEncoder()
-	encoder.Int32(0)                           // placeholder for message length
-	encoder.Int32(requestHeader.CorrelationID) // correlation_id
-	if len(supportedAPIVersions) == 0 || !contains(supportedAPIVersions, int16(requestHeader.ApiVersion)) {
-		encoder.Int16(35) // error_code: UNSUPPORTED_VERSION
-	} else {
+	encoder.Int32(0)                                   // placeholder for message length
+	encoder.Int32(requestHeader.CorrelationID)         // correlation_id
+	supportedAPIVersions := supportedAPIVersionMap[18] // Get the supported versions for API key 18 (ApiVersions)
+	if contains(supportedAPIs, int16(requestHeader.ApiKey)) &&
+		requestHeader.ApiVersion >= supportedAPIVersions[0] &&
+		requestHeader.ApiVersion <= supportedAPIVersions[1] {
 		encoder.Int16(0) // error_code: NO_ERROR
+	} else {
+		encoder.Int16(35) // error_code: UNSUPPORTED_VERSION
 	}
-	encoder.Uint8(2)                                                 // api_keys array length: 1 element
-	encoder.Int16(18)                                                // api_key: 18 (ApiVersions)
-	encoder.Int16(supportedAPIVersions[0])                           // min_version: 0
-	encoder.Int16(supportedAPIVersions[len(supportedAPIVersions)-1]) // max_version: 4
-	encoder.Uint8(0)
+
+	encoder.Uint8(uint8(len(supportedAPIs) + 1))
+	for _, apiKey := range supportedAPIs {
+		encoder.Int16(apiKey)
+		supportedVersions := supportedAPIVersionMap[apiKey]
+		encoder.Int16(supportedVersions[0])
+		encoder.Int16(supportedVersions[1])
+		encoder.Uint8(0) // TAG_BUFFER: empty
+	}
+
 	encoder.Int32(0)
 	encoder.Uint8(0) // TAG_BUFFER: empty
 
