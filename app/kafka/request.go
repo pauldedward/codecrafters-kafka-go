ad59b889ba00f2534bad76dee6794d263cb91fa7
Paul D Edward <edward2000ed@gmail.com>
2026-06-26 08:52:15 +0530

    codecrafters submit [skip ci]

--- app/kafka/request.go
+++ app/kafka/request.go
@@ -8,6 +8,7 @@ type RequestHeader struct {
 	ApiKey        int16
 	ApiVersion    int16
 	CorrelationID int32
+	ClientID      string
 	TagBuffer     byte
 }
 
@@ -33,9 +34,26 @@ func ParseHeader(decoder *decoder.Decoder) (RequestHeader, error) {
 		return RequestHeader{}, err
 	}
 
+	clientIDLength, err := decoder.Int16()
+	if err != nil {
+		return RequestHeader{}, err
+	}
+
+	clientID, err := decoder.String(int(clientIDLength))
+	if err != nil {
+		return RequestHeader{}, err
+	}
+
+	tagBuffer, err := decoder.Uint8()
+	if err != nil {
+		return RequestHeader{}, err
+	}
+
 	return RequestHeader{
 		ApiKey:        apiKey,
 		ApiVersion:    apiVersion,
 		CorrelationID: correlationID,
+		ClientID:      clientID,
+		TagBuffer:     tagBuffer,
 	}, nil
 }
